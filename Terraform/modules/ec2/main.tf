ffd97c3130c1daf48f5a6977b209b8ca05e2d562
bonyubking <piusku2@hanyang.ac.kr>
2025-11-27 08:42:39 -0800

    123

--- Terraform/modules/ec2/main.tf
+++ Terraform/modules/ec2/main.tf
@@ -9,28 +9,8 @@ resource "aws_instance" "servers" {
   vpc_security_group_ids      = [each.value.sg]
   associate_public_ip_address = each.value.public
 
-  source_dest_check = each.key == "nat" ? false : true
-
   tags = {
     Name = "${var.prefix}-${each.key}"
   }
-}
-
-resource "local_file" "ansible_inventory" {
-  filename = "${path.module}/../Ansible/inventory/hosts.ini"
-
-  content = <<-EOT
-[bastion]
-${aws_instance.servers["bastion"].public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/mykey.pem
-
-[public]
-${aws_instance.servers["public"].public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/mykey.pem
-
-[private]
-${aws_instance.servers["private"].private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/mykey.pem ansible_ssh_common_args='-o ProxyCommand="ssh -W %h:%p -i ~/.ssh/mykey.pem ec2-user@${aws_instance.servers["bastion"].public_ip}"'
-
-[nat]
-${aws_instance.servers["nat"].public_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/mykey.pem
-EOT
 }
 
