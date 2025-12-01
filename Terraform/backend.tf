00b6f014584a6f8820ce3c391914a992a9934c7f
구본엽 <piusku2@gmail.com>
2025-12-01 13:24:18 +0900

    terrra

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,4 +1,15 @@
 terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.4"
+    }
+  }
+
   backend "s3" {
     bucket         = "myce-terraform-state-bucket"
     key            = "terraform.tfstate"
